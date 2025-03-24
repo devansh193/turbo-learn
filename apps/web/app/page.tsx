48a74dd74fb5d68c118a92cf22f3997b7c75894a
devansh <devanshverma193@gmail.com>
2025-03-24 16:33:30 +0530

    web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,6 +6,9 @@ export default async function Home() {
     <div>
       <div>
         <h1>Hi there </h1>
+        <h1 className="text-xl font-sans">this is the name line</h1>
+        <h1>This is new line</h1>
+        <p>This is another line</p>
         <h1>{user?.name}</h1>
         <h1>{user?.password}</h1>
       </div>
